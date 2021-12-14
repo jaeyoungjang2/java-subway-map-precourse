00638f9c8aba84e8fe0ba08a553e15195793f8cc
jaeyoungjang2 <lion85488@gmail.com>
2021-12-14 12:59:33 +0900

    feat: 지하철 호선 초기화

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -2,6 +2,8 @@ package subway;
 
 import java.util.Scanner;
 import subway.controller.StationController;
+import subway.domain.Line;
+import subway.domain.LineRepository;
 import subway.domain.Station;
 import subway.domain.StationRepository;
 import subway.view.InputView;
@@ -11,11 +13,36 @@ public class Application {
         final Scanner scanner = new Scanner(System.in);
         StationController stationController = new StationController(scanner);
         initStation();
+        initLine();
 
         String mainSelect = InputView.requireMain(scanner);
         if (mainSelect.equals("1")) {
             stationController.run();
         }
+    }
+
+    private static void initLine() {
+        //- 2호선: 교대역 - 강남역 - 역삼역
+        //- 3호선: 교대역 - 남부터미널역 - 양재역 - 매봉역
+        //- 신분당선: 강남역 - 양재역 - 양재시민의숲역
+        Line line = new Line("2호선");
+        line.addStation("교대역");
+        line.addStation("강남역");
+        line.addStation("역삼역");
+        LineRepository.addLine(line);
+
+        line = new Line("3호선");
+        line.addStation("교대역");
+        line.addStation("남부터미널역");
+        line.addStation("양재역");
+        line.addStation("매봉역");
+        LineRepository.addLine(line);
+
+        line = new Line("신분당선");
+        line.addStation("강남역");
+        line.addStation("양재역");
+        line.addStation("양재시민의숲역");
+        LineRepository.addLine(line);
     }
 
     private static void initStation() {
