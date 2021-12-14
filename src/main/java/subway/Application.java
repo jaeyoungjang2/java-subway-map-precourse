3e81b0a7f0878c2143a61fbcf2474a5d3b176534
jaeyoungjang2 <lion85488@gmail.com>
2021-12-14 12:28:01 +0900

    feat: 메인 인풋을 사용자로부터 받아오기

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,15 @@
 package subway;
 
 import java.util.Scanner;
+import subway.controller.StationController;
+import subway.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
+        StationController stationController = new StationController();
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+
+        String mainSelect = InputView.requireMain(scanner);
+
     }
 }
